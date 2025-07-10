04225a0b3147ebfc798ea841d7e88d80826e9f10
DevionRosa <devionrosa@gmail.com>
2025-07-10 11:39:03 -0400

    Updated Websiteapp

--- websiteapp.py
+++ websiteapp.py
@@ -1,10 +1,18 @@
 # webpage.py
-from flask import Flask
+import git
+from flask import Flask, render_template, url_for, flash, redirect, request
 from flask_debugtoolbar import DebugToolbarExtension
 
 app = Flask(__name__) # Here's your 'app' instance
 
 # ... (rest of your code)
 
-if __name__ == '__main__':
-    app.run(debug=True, host="0.0.0.0")
+@app.route("/update_server", methods=['POST'])
+def webhook():
+    if request.method == 'POST':
+        repo = git.Repo('/home/ProjectWebsite/ProjectWebsite')
+        origin = repo.remotes.origin
+        origin.pull()
+        return 'Updated PythonAnywhere successfully', 200
+    else:
+        return 'Wrong event type', 400
